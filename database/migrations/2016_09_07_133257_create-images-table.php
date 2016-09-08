5d6fa8050cb3e9305e2431c72dd12062166ca424
Vermillion <tjc0609@gmail.com>
2016-09-07 20:36:07 -0700

    Added new migrations

---
+++ database/migrations/2016_09_07_133257_create-images-table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('imageId')->index();
+            $table->integer('userId');
+            $table->integer('noteId');
+            $table->string('imageName');
+            $table->string('imageUrl')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
